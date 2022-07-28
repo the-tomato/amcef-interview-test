54c477000d7512a816023b918299bf5d5a255bb8
xdubrav <xdubrav@fi.muni.cz>
2022-07-28 18:03:07 +0200

    fixed db file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import jsonify, current_app, g
 from pyms.flask.app import Microservice
-from database import db
+from database import db, Post
 
 ms = Microservice()
 app = ms.create_app()
@@ -8,8 +8,5 @@ app = ms.create_app()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 if __name__ == '__main__':
     app.run()
-    db.init_app(app)
-    db.create_all()
